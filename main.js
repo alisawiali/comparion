a122a78f4a9ad0807fccd87a8e73e6ca27ee62bc
alisawiali <alisawiali70@gmail.com>
2023-01-06 12:20:58 +0100

    text zentriert

--- main.js
+++ main.js
@@ -36,3 +36,60 @@ console.log( 30 <= 30);
 
 
 console.log( "Omer".length === "Imad".length);
+
+
+
+
+console.log("------Logical Operator---")
+
+/*
+
+Logical Operators منطقي
+-- ! Not 
+-- && And 
+-- || or
+
+*/
+
+console.log(true); // true 
+console.log(!true); // false
+console.log(10 == "10"); // true
+console.log(!(10 == "10"));  //false
+
+console.log(10 == "10" && 10 > 8  || 10 > 10);
+
+console.log(100  !== "100" || 10 >= 12);
+console.log( 100 !== "100"  && 20 < 30);
+
+
+
+
+
+console.log("-----if conditions -------")
+let price = 100;
+let dicount = true;
+let country  = "Deutland";
+let heimat = "berlin";
+let rabat = 20;
+let übernachten = "drei tage";
+let age = 21;
+
+if (dicount === false ){
+    price -=  rabat;
+}else if( heimat !== "berlin"){
+
+    if( age >= 21 && übernachten === "vier tage"){
+        price -= rabat;
+    }else ( age === 21)
+    price -= rabat;
+} else if ( country !== "Deutland"){
+    price -= rabat  +30 ;
+}
+else if( dicount === true){
+    price -=  rabat;
+}
+console.log(price);
+
+console.log("----Nested if conditions-------")
+
+// nesdet bedeutet machen wir  if condition in dem if  z.b ^
